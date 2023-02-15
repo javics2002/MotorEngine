e3341d936ef69d8c381cfc0b2651b3c8280d642d
javics2002 <49459590+javics2002@users.noreply.github.com>
2023-02-15 11:28:48 +0100

    MotorEngine :car: :cloud:

---
+++ Src/Main/main.cpp
@@ -0,0 +1,18 @@
+// Motor.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <fstream>
+
+#include "Ogre/Ogre.h"
+
+using namespace std;
+
+int main()
+{
+    saluda("Diego");
+    cout << "MotorEngine listo!\n";
+
+    return 0;
+}
+
---
+++ Src/Ogre/Ogre.cpp
@@ -0,0 +1,8 @@
+#include <OgreRoot.h>
+#include <iostream>
+
+void saluda(const char* name) {
+	Ogre::Root* root;
+	root = new Ogre::Root();
+	std::cout << "Ogre cargando Correctamente: " << name << std::endl;
+}
