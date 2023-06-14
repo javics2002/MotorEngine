32cbb5a37338f94faa8e308052a28ecc82358aeb
josemiguelvdz <josemiguelv2002@gmail.com>
2023-06-14 16:11:46 +0200

    Update build_Lua.bat

--- Dependencies/Lua/build_Lua.bat
+++ Dependencies/Lua/build_Lua.bat
@@ -1,6 +1,14 @@
 @echo off
 
+set "origen=.\Src\lib\Release\" 
+set "destino=.\bin\Release\" 
+
 cd .\Src
 
 msbuild "lua.sln" /p:configuration=Debug
 msbuild "lua.sln" /p:configuration=Release
+
+cd..
+
+rem Copia los binarios .dll
+robocopy /NJH %origen% %destino% *.dll
