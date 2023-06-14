6715b48a66d2a4f7e22b8d3d5452cdc4444d2a9e
josemiguelvdz <josemiguelv2002@gmail.com>
2023-06-14 13:32:38 +0200

    Merge branch 'main' of https://github.com/javics2002/P3-Motor

---
+++ Dependencies/Lua/build_Lua.bat
@@ -0,0 +1,10 @@
+@echo off
+set EXES=.\..\..\Exe
+
+cd .\Src
+
+msbuild "lua.sln" /p:configuration=Debug
+msbuild "lua.sln" /p:configuration=Release
+
+rem XCOPY /y /s .\lib\Debug\lua_d.dll %EXES%
+rem XCOPY /y /s .\lib\Release\lua.dll %EXES%
